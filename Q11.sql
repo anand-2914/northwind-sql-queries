210dbec11505cd96529260353c6764fbbd64eccc
Anand Singh <100718925+anand-2914@users.noreply.github.com>
2024-03-05 13:12:53 +0530

    11. Calculate the average product price by category

--- Q11.sql
+++ Q11.sql
@@ -1,10 +1,6 @@
-
- --12
-
-SELECT orders.employee_id, 
-       SUM((order_details.quantity * products.unit_price )- order_details.discount) AS total_revenue
-  FROM order_details
- INNER JOIN orders ON order_details.order_id = orders.order_id
- INNER JOIN products ON order_details.product_id = products.product_id
- GROUP BY orders.employee_id;
-
+--11  
+   SELECT categories.category_name, AVG(products.unit_price) AS avgs
+FROM categories 
+INNER JOIN products
+ON categories.category_id = products.category_id
+GROUP BY  categories.category_name;
