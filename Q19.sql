848871a79680f340c6edeaeddb926f006ee2bc70
Anand Singh <100718925+anand-2914@users.noreply.github.com>
2024-03-05 13:16:57 +0530

    19 Find the customers who have spent more than $5000 in total

--- Q19.sql
+++ Q19.sql
@@ -1,5 +1,5 @@
 
-  --19 Find the customers who have spent more than $5000 in total
+  ---19 Find the customers who have spent more than $5000 in total
   select orders.customer_id,order_details.quantity*products.unit_price-(order_details.discount) AS TOTAL  from orders
   inner join order_details on orders.order_id=order_details.order_id
   inner join products on  products.product_id=order_details.product_id
