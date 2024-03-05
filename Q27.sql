0c608601b6b3f9f96f49350ec435e57cfe073314
Anand Singh <100718925+anand-2914@users.noreply.github.com>
2024-03-05 13:19:55 +0530

    27. Find the average quantity of products ordered in each order

--- Q27.sql
+++ Q27.sql
@@ -1,5 +1,5 @@
 
-  --27. Find the average quantity of products ordered in each order
+  ---27. Find the average quantity of products ordered in each order
   select products.product_name,AVG(order_details.quantity) AS avgs from products
   inner join order_details
   on order_details.product_id=products.product_id
