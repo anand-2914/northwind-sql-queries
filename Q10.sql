db0f00cf2713e06219bcdb84455b8e388ce67994
Anand Singh <100718925+anand-2914@users.noreply.github.com>
2024-03-05 13:12:19 +0530

    10. List all ‘Orders’ with ‘Customer’ details and ‘Employee’ who processed it

--- Q10.sql
+++ Q10.sql
@@ -1,9 +1,7 @@
 
-  ---11  
-   SELECT categories.category_name, AVG(products.unit_price) AS avgs
-FROM categories 
-INNER JOIN products
-ON categories.category_id = products.category_id
-GROUP BY  categories.category_name;
 
-
+--10
+SELECT o.order_id, o.order_date, c.company_name AS customer_name, e.first_name AS employee_first_name, e.last_name AS employee_last_name
+FROM orders o
+JOIN customers c ON o.customer_id = c.customer_id
+JOIN employees e ON o.employee_id = e.employee_id;
