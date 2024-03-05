91e763f45cbf48469defe61b8dcbcf0e9da854bb
Anand Singh <100718925+anand-2914@users.noreply.github.com>
2024-03-05 13:16:03 +0530

    17. List all the distinct countries to which orders have been shipped

--- Q17.sql
+++ Q17.sql
@@ -1,5 +1,5 @@
 
---17List all the distinct countries to which orders have been shipped
+--17 List all the distinct countries to which orders have been shipped
 Select distinct ship_country from orders
 
 
