b037bb345896fe88bec9264a541fda10114df407
Anand Singh <100718925+anand-2914@users.noreply.github.com>
2024-03-05 13:21:44 +0530

    --32 Find the employees who have not processed any orders

--- Q32.sql
+++ Q32.sql
@@ -1,6 +1,6 @@
  
   
-  --32 Find the employees who have not processed any orders 
+  ---32 Find the employees who have not processed any orders 
   SELECT employees.employee_id, COUNT(orders.order_id) AS total
 FROM employees
 LEFT JOIN orders ON orders.employee_id = employees.employee_id
