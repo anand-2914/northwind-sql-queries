3cc8c02d5a2f4377fcdf67d1ca9c4aa2922a26af
Anand Singh <100718925+anand-2914@users.noreply.github.com>
2024-03-05 13:18:16 +0530

    23. Find the most common shipping country

--- Q23.sql
+++ Q23.sql
@@ -1,5 +1,5 @@
 
---23 23. Find the most common shipping country
+--23  Find the most common shipping country
 SELECT ship_country,COUNT(ship_country)AS country_name from orders 
 group by ship_country
  ORDER BY country_name DESC
