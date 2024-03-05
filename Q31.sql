f479016801222e0e0aad95b313e3a005ce7f79fd
Anand Singh <100718925+anand-2914@users.noreply.github.com>
2024-03-05 13:21:25 +0530

    31. Find the top 3 customers who have ordered the most products

--- Q31.sql
+++ Q31.sql
@@ -1,5 +1,5 @@
 
-  --31 31. Find the top 3 customers who have ordered the most products
+  --31 Find the top 3 customers who have ordered the most products
   
   select customers.contact_name,count(orders.order_id) as total_order from customers
   inner join orders on orders.customer_id=customers.customer_id
