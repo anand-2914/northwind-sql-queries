1905808b80b93a4ed665a1019e30625a241f4de2
Anand Singh <100718925+anand-2914@users.noreply.github.com>
2024-03-05 13:24:18 +0530

    38 Classify employees based on the number of orders they have processed such that NumberOfOrders > 100  Then PerformanceCategory as ‘High Performing’. If NumberOfOrders>50 Then ‘Medium Performing’ and else ‘Lower Performing’ S

--- Q38.sql
+++ Q38.sql
@@ -1,6 +1,6 @@
 
 
---38 Classify employees based on the number of orders they have processed such that NumberOfOrders > 100 
+---38 Classify employees based on the number of orders they have processed such that NumberOfOrders > 100 
 --Then PerformanceCategory as ‘High Performing’. If NumberOfOrders>50 Then ‘Medium Performing’ and else ‘Lower Performing’
 SELECT employees.first_name, 
        COUNT(DISTINCT orders.order_id) AS NoofOrders, 
